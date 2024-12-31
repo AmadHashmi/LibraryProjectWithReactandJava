7582dd0f11ad409efad3fb6ee7efc2e6ab071177
Amad Ul Hassan <ah@indigilabs.com>
2024-12-31 15:05:44 +0100

    added the configuration to make the http methods readonly
    added the configuration to make the http methods readonly

--- backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
@@ -1,4 +1,46 @@
 package com.luv2code.spring_boot_library.entity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name = "book")
+@Data
 public class Book {
+    @JsonProperty("id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @JsonProperty("title")
+    @Column(name = "title")
+    private String title;
+
+    @JsonProperty("author")
+    @Column(name = "author")
+    private String author;
+
+
+
+    @JsonProperty("description")
+    @Column(name = "description")
+    private String description;
+
+
+    @JsonProperty("copies")
+    @Column(name = "copies")
+    private int copies;
+
+    @JsonProperty("copies_available")
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+
+    @JsonProperty("category")
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
 }
