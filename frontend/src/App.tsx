f19c6b4a4b7b549a860adc7ac5ba4882a8234ad4
Amad Ul Hassan <ah@indigilabs.com>
2025-01-03 23:55:44 +0100

    added route for home and search
    added route for home and search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
-// import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { HomePage } from "./layouts/HomePage/HomePage";
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
